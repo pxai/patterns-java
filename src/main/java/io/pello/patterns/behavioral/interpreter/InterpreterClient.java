c8aa0328c18752f929625c1564b053d55c15be94
pxai <pello_altadill@cuatrovientos.org>
2017-07-04 08:45:45 +0200

    Ok, added interpreter

--- src/main/java/io/pello/patterns/behavioral/interpreter/InterpreterClient.java
+++ src/main/java/io/pello/patterns/behavioral/interpreter/InterpreterClient.java
@@ -1,5 +1,7 @@
 package io.pello.patterns.behavioral.interpreter;
 
+import java.util.ArrayList;
+
 /**
  * Client class to make use of the interpreter pattern example
  * @author Pello Xabier Altadill Izura
@@ -7,6 +9,14 @@ package io.pello.patterns.behavioral.interpreter;
  */
 public class InterpreterClient {
 	public static void main (String args[]) {
+		ArrayList<Order> orders = new ArrayList<Order>(); 
 		
+		orders.add(new Order(new Command("Move"),new Number(5)));
+		orders.add(new Order(new Command("Attack"),new Item("enemy")));
+		orders.add(new Order(new Command("Defend"),new Item("town")));
+		
+		for (Order order : orders) {
+			System.out.println(order.interpret());
+		}
 	}
 }
